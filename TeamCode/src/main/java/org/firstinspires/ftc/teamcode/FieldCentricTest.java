b25450355bd4e7dd08333807cda24fa0e97e828f
kdodds6 <kmdodds9@gmail.com>
2021-12-03 20:37:42 -0500

    changed arm values to have doubled sided d-pad changed intake buttons Still need to figure out logic and front side of d-pad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricTest.java
@@ -50,6 +50,7 @@ public class FieldCentricTest extends LinearOpMode {
         double Horizontal = 0;
         double Vertical = 0;
         double Piviot = 0;
+        double ArmPower = 0.75;
 
 
         //Mapping Config objects to variables(Below)
@@ -130,37 +131,81 @@ public class FieldCentricTest extends LinearOpMode {
                 LeftRear.setPower(Piviot + (ModdedVertical - ModdedHorizontal));
                 //setting powers
             }
-            if (gamepad1.a) {
-                //button a = arm to (NEED TO RE- MEASURE VALUES)
-                Arm.setPower(0.5);
+
+
+            double RightTrigger = gamepad1.right_trigger;
+
+            if (gamepad1.dpad_down) {
+                //arm = Floor (Back)
+                Arm.setPower(ArmPower);
                 Arm.setTargetPosition(0);
-            } else if (gamepad1.b) {
-                //button b = arm to
-                Arm.setPower(0.75);
-                Arm.setTargetPosition(2300);
-            } else if (gamepad1.y) {
-                //button y = arm to
-                Arm.setPower(0.75);
-                Arm.setTargetPosition(2577);
-            } else if (gamepad1.x) {
-                //button x = arm to
-                Arm.setPower(0.75);
-                Arm.setTargetPosition(2775);
-            } else if (gamepad1.right_bumper) {
-                //Right Bumper = turn off arm
-                Arm.setPower(0);
-            } else if (gamepad1.dpad_left) {
-                //Dpad left = activates outake
-                StarIntake.setDirection(DcMotorSimple.Direction.FORWARD);
-                StarIntake.setPower(0.5);
-            } else if (gamepad1.dpad_right) {
-                //Dpad right = activate intake
+                telemetry.addData("Arm Position", Arm.getCurrentPosition());
+                telemetry.addData("down",1);
+                telemetry.update();
+            }
+            else if (gamepad1.dpad_right) {
+                //arm = 1st tier (Back)
+                Arm.setPower(ArmPower);
+                Arm.setTargetPosition(380);
+                telemetry.addData("Arm Position", Arm.getCurrentPosition());
+                telemetry.addData("right",1);
+                telemetry.update();
+            }
+            else if (gamepad1.dpad_left) {
+                //arm = 2nd tier (Back)
+                Arm.setPower(ArmPower);
+                Arm.setTargetPosition(760);
+                telemetry.addData("Arm Position", Arm.getCurrentPosition());
+                telemetry.addData("left",1);
+                telemetry.update();
+            }
+            else if (gamepad1.dpad_up) {
+                //arm= 3rd tier (Back)
+                Arm.setPower(ArmPower);
+                Arm.setTargetPosition(1000);
+                //760-1000
+                telemetry.addData("Arm Position", Arm.getCurrentPosition());
+                telemetry.addData("up",1);
+                telemetry.update();
+            }
+             else if (gamepad1.dpad_down &= gamepad1.right_trigger > 0.3) {
+                //arm = Floor (Front)
+                Arm.setPower(ArmPower);
+                Arm.setTargetPosition(3100);
+            }
+            else if (gamepad1.dpad_right && gamepad1.right_trigger > 0.3) {
+                //arm = 1st tier (Front)
+                Arm.setPower(ArmPower);
+                Arm.setTargetPosition(2862);
+            }
+            else if (gamepad1.dpad_left && RightTrigger > 0.3) {
+                //arm = 2nd tier (Front)
+                Arm.setPower(ArmPower);
+                Arm.setTargetPosition(2517);
+            }
+            else if (gamepad1.dpad_up && RightTrigger  > 0.3) {
+                //arm = 3rd tier (Front)
+                Arm.setPower(ArmPower);
+                Arm.setTargetPosition(2100);
+            }
+
+
+            telemetry.addData("Arm Position", Arm.getCurrentPosition());
+            telemetry.addData("TRIGGER VALUE", RightTrigger);
+            telemetry.update();
+
+            if(gamepad1.a) {
+                //A = activates intake
                 StarIntake.setDirection(DcMotorSimple.Direction.REVERSE);
                 StarIntake.setPower(0.5);
             }
-            telemetry.addData("Arm Position", Arm.getCurrentPosition());
-            telemetry.update();
-            //telemetry for de-bugging
+            else if(gamepad1.b) {
+                //B = activate outake
+                StarIntake.setDirection(DcMotorSimple.Direction.FORWARD);
+                StarIntake.setPower(0.5);
+            }
+
+
         }
     }
 }
